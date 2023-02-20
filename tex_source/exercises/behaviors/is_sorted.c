490a199a4572f7225e428188a00e05a9f89ba4c3
Nikolaus Huber <mail@n-huber.eu>
2023-02-20 09:31:46 +0100

    Added new example in behaviors and removed alphanumeric example.

---
+++ tex_source/exercises/behaviors/is_sorted.c
@@ -0,0 +1,16 @@
+int is_sorted(int* arr, int len) 
+{
+    
+}
+
+int main()
+{
+    int a1[] = {1,2,3};
+    int a2[] = {2,1,3}; 
+
+    int r1 = is_sorted(a1, 3); 
+    int r2 = is_sorted(a2, 3); 
+
+    //@ assert r1 == 1;
+    //@ assert r2 == 0; 
+}
